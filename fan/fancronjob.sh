b79614dfc7e5d3d4b76f25145abf2b4663d38029
gonzaloroagutierrez <64004002+gonzaloroagutierrez@users.noreply.github.com>
2020-09-29 21:50:40 +0200

    Add files via upload

---
+++ fan/fancronjob.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [ ! -f "/tmp/mode.txt" ]; then echo "3" > /tmp/mode.txt; fi
+
+MODE=$(head -1 /tmp/mode.txt)
+#0=quiet, 1=normal, 2=loud
+QUIET_MODE_MAX=45
+NORMAL_MODE_MIN=47
+NORMAL_MODE_MAX=67
+LOUD_MODE_MIN=70
+
+
+OUTPUT=$(sensors | grep "Package id 0:" | cut -d " " -f 5 | cut -d "+" -f 2 | cut -d "." -f 1)
+OUTPUT=${OUTPUT%"°C"}
+
+if [ $OUTPUT -le $QUIET_MODE_MAX ] && [ $MODE -ne 0 ]
+then
+	echo "T<40 and Mode=Not silent"
+	sudo python3 /opt/msiprestige15/fan/control.py /opt/msiprestige15/fan/quiet.ini &
+	MODE=0
+	echo "0" > /tmp/mode.txt
+	echo "*********************** Quiet mode activated"
+fi
+
+if [ $OUTPUT -ge $LOUD_MODE_MIN ] && [ $MODE -ne 2 ]
+then
+	echo "T>70 and Mode=Not loud"
+	sudo python3 /opt/msiprestige15/fan/control.py /opt/msiprestige15/fan/loud.ini &
+	MODE=2
+	echo "2" > /tmp/mode.txt
+	echo "*********************** Loud mode activated"
+fi
+
+if [ $OUTPUT -ge $NORMAL_MODE_MIN ] && [ $OUTPUT -le $NORMAL_MODE_MAX ] && [ $MODE -ne 1 ]
+then
+	echo "T>41 & T<69 and Mode=Not normal"
+	sudo python3 /opt/msiprestige15/fan/control.py /opt/msiprestige15/fan/normal.ini &
+	MODE=1
+	echo "1" > /tmp/mode.txt
+	echo "*********************** Normal mode activated"
+fi
