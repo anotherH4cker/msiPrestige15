59595fbbc475a74a0c1de7e9d2cbf4703609a412
anotherH4cker <64004002+anotherH4cker@users.noreply.github.com>
2021-09-01 16:37:06 +0200

    Update fancronjob.sh

--- fan/fancronjob.sh
+++ fan/fancronjob.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f "/tmp/mode.txt" ]; then echo "3" > /tmp/mode.txt; fi
 
-MODE=$(head -1 /tmp/mode.txt)
+MODE=1
 #0=quiet, 1=normal, 2=loud
 QUIET_MODE_MAX=45
 NORMAL_MODE_MIN=47
@@ -10,39 +10,45 @@ NORMAL_MODE_MAX=67
 LOUD_MODE_MIN=70
 
 
-OUTPUT=$(sensors | grep "Package id 0:" | cut -d " " -f 5 | cut -d "+" -f 2 | cut -d "." -f 1)
-#OUTPUT=${OUTPUT%"°C"}
-
-counter=1
+counter=0
+counter2=0
 while [ $counter -le 10 ]
 do
-	if [ $OUTPUT -le $QUIET_MODE_MAX ] && [ $MODE -ne 0 ]
-	then
-		echo "T<40 and Mode=Not silent"
-		sudo python3 /opt/fancontrol/fan/control.py /opt/fancontrol/fan/quiet.ini &
-		MODE=0
-		echo "0" > /tmp/mode.txt
-		echo "*********************** Quiet mode activated"
-	fi
+	while [ $counter2 -le 5 ] 
+	do
+		OUTPUT=$(sensors | grep "Package id 0:" | cut -d " " -f 5 | cut -d "+" -f 2 | cut -d "." -f 1)
 
-	if [ $OUTPUT -ge $NORMAL_MODE_MIN ] && [ $OUTPUT -le $NORMAL_MODE_MAX ] && [ $MODE -ne 1 ]
-	then
-		echo "T>41 & T<69 and Mode=Not normal"
-		sudo python3 /opt/fancontrol/fan/control.py /opt/fancontrol/fan/normal.ini &
-		MODE=1
-		echo "1" > /tmp/mode.txt
-		echo "*********************** Normal mode activated"
-	fi
+		if [ $OUTPUT -le $QUIET_MODE_MAX ] && [ $MODE -ne 0 ]
+		then
+			#echo "T<40 and Mode=Not silent"
+			sudo python3 /opt/fancontrol/fan/control.py /opt/fancontrol/fan/quiet.ini &
+			MODE=0
+		fi
 
-	if [ $OUTPUT -ge $LOUD_MODE_MIN ] && [ $MODE -ne 2 ]
-	then
-		echo "T>70 and Mode=Not loud"
-		sudo python3 /opt/fancontrol/fan/control.py /opt/fancontrol/fan/loud.ini &
-		MODE=2
-		echo "2" > /tmp/mode.txt
-		echo "*********************** Loud mode activated"
-	fi
-	sleep 1
+		if [ $OUTPUT -ge $NORMAL_MODE_MIN ] && [ $OUTPUT -le $NORMAL_MODE_MAX ] && [ $MODE -ne 1 ]
+		then
+			#echo "T>41 & T<69 and Mode=Not normal"
+			sudo python3 /opt/fancontrol/fan/control.py /opt/fancontrol/fan/normal.ini &
+			MODE=1
+		fi
+
+		if [ $OUTPUT -ge $LOUD_MODE_MIN ] && [ $MODE -ne 2 ]
+		then
+			#echo "T>70 and Mode=Not loud"
+			sudo python3 /opt/fancontrol/fan/control.py /opt/fancontrol/fan/loud.ini &
+			MODE=2
+		fi
+		
+		sleep 1
+
+
+		((counter2++))
+
+	done
+
+	MODE=1
+	counter2=0
+
 done
 
 
